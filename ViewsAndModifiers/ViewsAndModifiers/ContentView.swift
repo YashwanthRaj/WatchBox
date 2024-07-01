eb63ef2d7b3d1e87fa9cbe1279e05702f937140a
YashwanthRaj <yashwanthraj6383@gmail.com>
2024-07-01 12:15:09 -0400

    Project 1 Modification
    This version of the project will have the use of custom modifier which will change the color of total per person to red when zero tip is selected.

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -1,124 +1,88 @@
 //
 //  ContentView.swift
-//  ViewsAndModifiers
+//  WeSplit
 //
-//  Created by Yashwanth Raj Varadharajan on 6/29/24.
+//  Created by Yashwanth Raj Varadharajan on 6/24/24.
 //
 
 import SwiftUI
 
-struct CapsuleText: View {   // Create a custom view
-    var text: String
-    
-    var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .foregroundStyle(.white)
-            .background(.blue)
-            .clipShape(.capsule)
-    }
-}
-
-// Custom Modifier
-struct Title: ViewModifier {
+struct RedText: ViewModifier {
     func body(content: Content) -> some View {
-        content.font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
-
-    }
-}
-
-// We can also create extension that we can use in main body without modifier(Title())
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.black)
-        }
+        content.foregroundColor(.red)
     }
 }
 
 extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
+    func redZeroTip() -> some View {
+        modifier(RedText())
     }
 }
 
 struct ContentView: View {
-    @State private var useRedText = false
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
-    // Views as properties
-    let name1 = Text("Hello World!!")
-    let name2 = Text("My name is Yash")
+    let  tipPercentages = [10,15,20,25,0]
     
-    var name3: some View {
-        Text("I am a computer science student")
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
     }
     
     var body: some View {
-        Spacer()
-        
-        // Conditional Modifiers
-        Button("Button") {
-            useRedText.toggle()
+        NavigationStack {
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people:", selection: $numberOfPeople){
+                        ForEach(2..<100){ person in
+                            Text("\(person) people")
+                                
+                        }
+                    }
+                    //.pickerStyle(.navigationLink)
+                }
+                
+                Section("How much tip do you want to pay") {
+                    Picker("Tip Percentage:", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){ tip in
+                            Text(tip, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                
+                Section{
+                    if tipPercentage == 0 {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).redZeroTip()
+                    } else {
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                    
+                    
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .foregroundColor(useRedText ? .red: .blue )
-        
-        Spacer()
-        
-        // Environment Modifiers
-        VStack{
-            Text("Hello world!!")
-                .font(.largeTitle)  //  This will override the environment modifier
-            Text("Hello world!!")
-            Text("Hello world!!")
-            Text("Hello world!!")
-            Text("Hello world!!")
-        }.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/) // Will apply to all
-        
-        Spacer()
-        
-        // Views being used as properties
-        VStack{
-            name1.font(.title2)  // We can apply modifiers to them as well
-            name2
-        }.padding()
-        
-        Spacer()
-        
-        // Use the view here
-        CapsuleText(text: "Hello World")
-        
-        Spacer()
-        
-        // Using Custom Modifier
-        Text("Hello World").modifier(Title())
-        
-        // Using the extension
-        Text("Hello").titleStyle()
-        
-        Spacer()
-        
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Yashwanth")
-        Spacer()
     }
 }
 
